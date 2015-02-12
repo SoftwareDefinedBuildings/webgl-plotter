9eab2a679c1a184be342c6dde4fdcd81a0b5bb84
Sam Kumar <samkumar@berkeley.edu>
2015-02-11 20:28:01 -0800

    Made progress toward proxying data requests

--- serveplotter.go
+++ serveplotter.go
@@ -5,11 +5,14 @@ import (
 	"io/ioutil"
 	"log"
     "net/http"
+    "strconv"
+    "strings"
     "sync"
     
     cparse "github.com/SoftwareDefinedBuildings/sync2_quasar/configparser"
     cpint "github.com/SoftwareDefinedBuildings/quasar/cpinterface"
 	capnp "github.com/glycerine/go-capnproto"
+	uuid "code.google.com/p/go-uuid/uuid"
 )
 
 type QueryMessagePart struct {
@@ -40,11 +43,64 @@ func (drh DataRequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 	
+	// TODO: don't just read the whole thing in one go. Instead give up after a reasonably long limit.
+	payload, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf("Could not read received POST payload: %v", err)))
+	}
+	
+	var args []string = strings.Split(string(payload), ",")
+	
+	if len(args) != 4 {
+		w.Write([]byte(fmt.Sprintf("Four arguments are required; got %v", len(args))))
+		return
+	}
+	
+	var uuidBytes uuid.UUID = uuid.Parse(args[0])
+	
+	if uuidBytes == nil {
+		w.Write([]byte(fmt.Sprintf("Invalid UUID: got %v", args[0])))
+		return
+	}
+	
+	var startTime int64
+	var endTime int64
+	var pw int64
+	
+	startTime, err = strconv.ParseInt(args[1], 16, 64)
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf("Could not interpret %v as an int64: %v", args[1], err)))
+		return
+	}
+	
+	endTime, err = strconv.ParseInt(args[2], 16, 64)
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf("Could not interpret %v as an int64: %v", args[2], err)))
+		return
+	}
+	
+	pw, err = strconv.ParseInt(args[3], 16, 16)
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf("Could not interpret %v as an int16: 5v", args[3], err)))
+	}
+	
 	var mp QueryMessagePart = queryPool.Get().(QueryMessagePart)
 	
-	/*segment := mp.segment
+	// segment := mp.segment
 	request := mp.request
-	query := mp.query*/
+	query := mp.query
+	
+	query.SetUuid([]byte(uuidBytes))
+	query.SetVersion(0)
+	query.SetStartTime(startTime)
+	query.SetEndTime(endTime)
+	query.SetPointWidth(uint8(pw))
+	
+	request.SetEchoTag(0) // TODO: Change to something meaningful so that we can actually match it to multiple requests
+	
+	request.SetQueryStatisticalValues(*query)
+	
+	fmt.Println("Prepared message to send")
 	
 	queryPool.Put(mp)
 }

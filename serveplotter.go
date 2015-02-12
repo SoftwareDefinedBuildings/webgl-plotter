1024c556cfa79724cde048eaf2f510186f3f49e5
Sam Kumar <samkumar@berkeley.edu>
2015-02-11 17:53:16 -0800

    Read directory to serve from configuration file

--- serveplotter.go
+++ serveplotter.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
     "net/http"
+    
+    cparse "github.com/SoftwareDefinedBuildings/sync2_quasar/configparser"
 )
 
-/*type PlotRequestHandler struct {}
+type DataRequestHandler struct {}
 
-func (prh PlotRequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-}*/
+func (drh DataRequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		fmt.Println("You must send a POST request to get data.")
+		return
+	}
+}
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+	configfile, err := ioutil.ReadFile("plotter.ini")
+	if err != nil {
+		fmt.Printf("Could not read plotter.ini: %v\n", err)
+		return
+	}
+	
+	config, isErr := cparse.ParseConfig(string(configfile))
+    if isErr {
+        fmt.Println("There were errors while parsing plotter.ini. See above.")
+        return
+    }
+    
+	directory, ok := config["plotter_dir"]
+	if !ok {
+	    fmt.Println("Configuration file is missing required key \"plotter_dir\"")
+	    return
+	}
+	
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir(directory.(string)))))
 }
 
